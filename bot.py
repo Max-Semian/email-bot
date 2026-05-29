36f2a825375346e77486a89208779c295475d0e3
Max <gregory@rasputin.impr>
2026-05-29 17:46:12 +0700

    chore: silence http request logs

--- bot.py
+++ bot.py
@@ -30,6 +30,7 @@ load_dotenv()
 logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
